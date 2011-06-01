95fb248f1be17d72b873992b4e4abb2bbff831c5
keith.nicholas <keith.nicholas@c8fdfc7a-b1b0-2da2-59b2-95209285a470>
2011-06-01 04:23:53 +0000

    updated for nix

--- trunk/examples/nix/example_project.c
+++ trunk/examples/nix/example_project.c
@@ -22,9 +22,36 @@ void my_suite_teardown( void )
 
 int main( int argc, char** argv )
 {
+	return seatest_testrunner(argc, argv, all_tests, my_suite_setup, my_suite_teardown);	
+}
+
+/*************************************************************************************************************************************************************/
+/*  Everything after this point are just alternative "main" functions which show different ways you can run the tests....they don't get used in this example */
+/*************************************************************************************************************************************************************/
+
+/*
+Use this if you don't have any global setup/teardown...
+*/
+int main_no_setup_or_teardown( int argc, char** argv )
+{
+	return seatest_testrunner(argc, argv, all_tests, NULL, NULL);	
+}
+
+/*
+Use this if you don't want to use the test runner...
+*/
+int main_do_it_myself( int argc, char** argv )
+{
 	suite_setup(my_suite_setup);
-	suite_teardown(my_suite_teardown);
-	run_tests(all_tests);	
-	return 0;
+	suite_teardown(my_suite_teardown); 
+	return run_tests(all_tests);	
+}
+
+/*
+Use this if you don't want to use the test runner and don't have any global setup/teardown...
+*/
+int main_do_it_myself_really_simply( int argc, char** argv )
+{	
+	return run_tests(all_tests);	
 }
 
